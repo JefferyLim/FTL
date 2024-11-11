4b9cdb52c0815d5e5425804e9a3e6b5b96d7a83c
Jeffery Lim <jl+git@jefferyjlim.com>
2024-11-11 13:21:21 -0500

    Adding baud setter and message setter

--- software/transmitter/transmitter.ino
+++ software/transmitter/transmitter.ino
@@ -9,6 +9,8 @@ void setup() {
   pinMode(irPin, OUTPUT);
   Serial.begin(9600);
   myTimer.begin(blinkLED, 1500000);  // blinkLED to run every 0.15 seconds
+
+  usage();
 }
 
 // The interrupt will blink the LED, and keep
@@ -32,32 +34,114 @@ void blinkLED() {
 
 void usage(){
   Serial.println("FTL Transmitter");
-  Serial.println("h          - prints this message")
-  Serial.println("s          - stop transmitter")
-  Serial.println("b [number] - set baud rate ")
-  Serial.println("m [string] - set transmit message ")
+  Serial.println("h          - prints this message");
+  Serial.println("s          - stop transmitter");
+  Serial.println("b [number] - set baud rate ");
+  Serial.println("m [string] - set transmit message ");
+}
+
+const byte numChars = 16;
+char receivedChars[numChars];
+int parseInput(){
+  int i = 0;
+  char incomingByte;
+    
+  while (Serial.available() != 0){
+    incomingByte = Serial.read();
+    if (incomingByte != ' '){
+      receivedChars[i] = incomingByte;
+      i++;
+    }
+  }
+  receivedChars[i] = '\0';
+  Serial.println(receivedChars);
+  return atoi(receivedChars);
 }
 
 
+char* message;
+int messageLength;
+void parseMessage(){
+  int i = 0;
+  char incomingByte;
+  char tempChars[100];
+  
+  while (Serial.available() != 0 && i < 100){
+    incomingByte = Serial.read();
+    tempChars[i] = incomingByte;
+    i++;
+  }
+  tempChars[i] = '\0';
+
+  messageLength = i;
+  free(message);
+  message = (char*)malloc( sizeof(char) * ( messageLength + 1 ) );
+  while (i >= 0){
+    message[i] = tempChars[i];
+    i--;
+  }
+}
+
+void flushInput(){
+   while(Serial.available() > 0 ) Serial.read();
+}
+
+volatile bool halt = 0;
+volatile int baud = 9600;
 void loop() {
     char incomingByte;
-    usage();
-    
-    while (Serial.available()) {
-      incomingByte = Serial.read();
-      switch (incomingByte) {
-        case 's':  // your hand is on the sensor
-          Serial.println("stop");
-          break;
-        case 'b':  // your hand is close to the sensor
-          Serial.println("baud");
-          break;
-        case 'm':  // your hand is a few inches from the sensor
-          Serial.println("message");
-          break;
-        default:
-          usage();
-          break;
+    int new_baud;
+    int i = 0;
+    int j = 0;
+
+    while (Serial.available() != 0) {
+      if(Serial.available() < 16){
+        Serial.println(Serial.available());
+        incomingByte = Serial.read();
+        switch (incomingByte) {
+          case 's':  // start or stop transmitter
+            halt = !halt;
+            if(halt){
+              Serial.println("starting...");
+            }else{
+              Serial.println("stopping...");
+            }
+            break;
+          case 'b':  // set baud rate
+            new_baud = parseInput();
+            if(new_baud == 0){
+              Serial.println("ERROR: baud input should be a integer");
+            }else{
+              halt = 0;
+              Serial.print("baud set to ");
+              Serial.println(new_baud);
+              baud = new_baud;
+              halt = 1;
+            }
+            break;
+          case 'm':  // set new message
+            Serial.println("message");
+            parseMessage();
+            Serial.println(message);
+            for(i=0;i < messageLength; i++){
+                Serial.println(message[i]);
+              for(j=0; j < 8; j++){
+                Serial.println(bitRead(message[i],j));
+              }
+            }
+            break;
+          case '\n': 
+            break;
+          case '\r':  
+            break;
+          default:
+            usage();
+            break;
+        }
+        flushInput();
+      }else{
+        flushInput();
+        Serial.println("Input too long");
       }
   }
 
