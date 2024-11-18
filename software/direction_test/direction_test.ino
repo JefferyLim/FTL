fa0e4065dd0bd7ac2c38f68b0a47e765c06b34eb
Jeffery Lim <jl+git@jefferyjlim.com>
2024-11-18 00:11:48 -0500

    Cleanup
    - Removed unnecessary flags
    - Changed definitions to all caps
    - Renamed variables to make more sense

--- software/direction_test/direction_test.ino
+++ software/direction_test/direction_test.ino
@@ -3,11 +3,11 @@
 #include <RingBuf.h>
 #include <IntervalTimer.h>
 
-#define sensor_thresh 1000
-#define sensor_dif_thresh 100
+#define SENSOR_THRESH 1000
+#define SENSOR_DIF_THRESH 100
 
-#define baud_rate 1000
-double sensor_period = 1000000/baud_rate;
+#define SAMPLING_FREQ 20000
+double sampling_period = 1000000/SAMPLING_FREQ; // Convert Sampling Frequency (Hz) to period (us)
 
 //Timers (to emulate multithreading)
 IntervalTimer sensor_readTimer; 
@@ -33,13 +33,12 @@ enum message_state{
   SYNCH, 
 };
 
-
-volatile bool just_read = false;
 volatile int counter = 0;
 //Sensor variables
-int left;
-int mid;
-int right;
+uint16_t left;
+uint16_t mid;
+uint16_t right;
+
 //difference variables
 int edge_dif;
 int left_mid_dif;
@@ -49,7 +48,6 @@ tracking_state track_state = SCANNING;
 posistion_state pos_state;
 
 int last_x_samples;
-
 
 void setup() {
   pinMode(left_pin, INPUT);
@@ -70,32 +68,35 @@ void setup() {
   adc->adc1->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED); // change the conversion speed
   adc->adc1->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED); // change the sampling speed
 
-
   sensor_readTimer.priority(0); //Set sens  or priority higher
   posistionTimer.priority(1);
   
-  sensor_readTimer.begin(read_sensors,(int)sensor_period); //Read senors every 0.01 seconds
+  sensor_readTimer.begin(read_sensors,(int)sampling_period); //Read senors every 0.01 seconds
   //posistionTimer.begin(find_posistion,100000); //Process every 0.1 seconds
 }
 
-struct photodiode_read{
-  int left;
-  int mid;
-  int right;
+// Created photodiode array struct
+struct photodiode_array{
+  uint16_t left;
+  uint16_t mid;
+  uint16_t right;
 };
 
-RingBuf<photodiode_read, 500> myRingBuffer;
-struct photodiode_read dataset;
+// RingBuffer for ADC sampling
+RingBuf<photodiode_array, 500> myRingBuffer;
+struct photodiode_array dataset;
+volatile bool bufferOverflow = 0;
 
 void read_sensors() { //Read analog sensor input
   dataset.left = adc->adc0->analogRead(left_pin);
   dataset.mid = adc->adc1->analogRead(mid_pin);
   dataset.right = adc->adc0->analogRead(right_pin);
+
   if(!myRingBuffer.push(dataset)){
     Serial.println("ERROR: Can't push to buffer");
+    bufferOverflow = 1;
   }
 
-  counter++;
 }
 
 void find_posistion(){ //Determine posistion of transmitter
@@ -104,11 +105,11 @@ void find_posistion(){ //Determine posistion of transmitter
   left_mid_dif = left - mid;
   mid_right_dif = mid - right;
 
-  if(left > sensor_thresh && mid > sensor_thresh && right > sensor_thresh){ //If no good signal
+  if(left > SENSOR_THRESH && mid > SENSOR_THRESH && right > SENSOR_THRESH){ //If no good signal
     track_state = SCANNING; 
   }else{
     track_state = LOCKED;
-    if(abs(edge_dif - left_mid_dif) < sensor_dif_thresh){ //If left of diodes
+    if(abs(edge_dif - left_mid_dif) < SENSOR_DIF_THRESH){ //If left of diodes
       pos_state = LEFT;
     }
 
@@ -124,7 +125,7 @@ void find_posistion(){ //Determine posistion of transmitter
       pos_state = MID_RIGHT;
     }
 
-    if(abs(edge_dif - mid_right_dif) < sensor_dif_thresh && right < left){ //If right of diodes
+    if(abs(edge_dif - mid_right_dif) < SENSOR_DIF_THRESH && right < left){ //If right of diodes
       pos_state = RIGHT;
     }
   }
@@ -179,17 +180,17 @@ int last_counter = 0;
 
 void loop() {
   bool current_bit = false;
-  struct photodiode_read readdata;
+  struct photodiode_array adcValues;
 
-  if(myRingBuffer.pop(readdata)){
-    left = readdata.left;
-    right = readdata.right;
-    mid = readdata.mid;
+  if(bufferOverflow){
+    Serial.println("ERROR: RingBuffer Overflow");
+  }
 
-    if(last_counter != counter - 1){
-      //Serial.println("ERROR");
-    }
-    last_counter = counter;
+  if(myRingBuffer.pop(adcValues)){
+    
+    left = adcValues.left;
+    right = adcValues.right;
+    mid = adcValues.mid;
 
     if(left + right + mid <= 3000){
       current_bit = true;
@@ -197,41 +198,40 @@ void loop() {
       current_bit = false;
     }
     //Serial.println(current_bit);
-    message_buf[i] = current_bit;
-    i++;
-    just_read = false;
+    // message_buf[i] = current_bit;
+    // i++;
   }
 
-  if(i == 1024){
-    sensor_readTimer.end();
-    myRingBuffer.clear();
-    //char ascii_string[128];
-    //int ascii_index = 0;
-    for(int j = 0; j < 1024; j++){
-      Serial.print(message_buf[j]);
-    }
-    // for(int j = 7; j < 1024; j = j + 8){
-    //   //char temp_byte;
-    //   int temp_sum = 0;
-    //   int multiplier = 0;
-    //   for(int k = j; k < j + 7; k++){
-    //     temp_sum += message_buf[k] * (1 << multiplier++);
-    //   }
-    //   ascii_string[ascii_index] = (char)temp_sum;
-    //   ascii_index++;
-    // }
+  // if(i == 1024){
+  //   sensor_readTimer.end();
+  //   myRingBuffer.clear();
+  //   //char ascii_string[128];
+  //   //int ascii_index = 0;
+  //   for(int j = 0; j < 1024; j++){
+  //     Serial.print(message_buf[j]);
+  //   }
+  //   // for(int j = 7; j < 1024; j = j + 8){
+  //   //   //char temp_byte;
+  //   //   int temp_sum = 0;
+  //   //   int multiplier = 0;
+  //   //   for(int k = j; k < j + 7; k++){
+  //   //     temp_sum += message_buf[k] * (1 << multiplier++);
+  //   //   }
+  //   //   ascii_string[ascii_index] = (char)temp_sum;
+  //   //   ascii_index++;
+  //   // }
 
-    // for(int f = 0; f < 128; f++){
-    //     if(ascii_string[f] == 0){
-    //       Serial.print("NULL");
-    //     }else{
-    //       Serial.print(ascii_string[f]);
-    //     }
-    // }
-    Serial.println();
-    delay(10000);
-    sensor_readTimer.begin(read_sensors,(int)sensor_period);
-  }
+  //   // for(int f = 0; f < 128; f++){
+  //   //     if(ascii_string[f] == 0){
+  //   //       Serial.print("NULL");
+  //   //     }else{
+  //   //       Serial.print(ascii_string[f]);
+  //   //     }
+  //   // }
+  //   Serial.println();
+  //   delay(10000);
+  //   sensor_readTimer.begin(read_sensors,(int)sampling_period);
+  // }
 
   //edge_difference = val0 - val2;
   //Serial.println(edge_difference);
@@ -242,7 +242,4 @@ void loop() {
   //}else{
   //  Serial.println("Stay still!");
   //}
-
-  delay(1);
-
 }
